ef3aafd1ed6bbdd26d9c65aba3a12b15618e16aa
Adam Reiter <adr5240@gmail.com>
2016-08-05 17:00:35 -0400

    Remove Minification warning from Heroku

---
+++ webpack.config.prod.js
@@ -0,0 +1,39 @@
+var webpack = require("webpack");
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: path.join(__dirname, 'frontend', 'better_reads.jsx'),
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
