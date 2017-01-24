75677470074ffbb1a317bfa462e3d026740431da
Adam Reiter <adr5240@gmail.com>
2017-01-24 14:23:33 -0500

    Refactor config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 
-  namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create] do
-      resources :bookshelves, except: [:edit, :new]
+    namespace :api, defaults: { format: :json } do
+        resources :users, only: [:create] do
+            resources :bookshelves, except: [:edit, :new]
+        end
+        resources :books, only: [:index, :show, :create, :update, :destroy] do
+            resources :reviews, only: [:index, :show, :create]
+        end
+        resources :authors, only: [:index, :show, :create]
+        resource :sessions, only: [:create, :destroy]
     end
-    resources :books, only: [:index, :show, :create, :update, :destroy] do
-      resources :reviews, only: [:index, :show, :create]
-    end
-    resources :authors, only: [:index, :show, :create]
-    resource :sessions, only: [:create, :destroy]
-  end
 
-  root to: 'static_pages#root'
+    root to: 'static_pages#root'
 end
