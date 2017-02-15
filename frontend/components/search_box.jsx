0d864d645f51580a2095bbd59cad472a96d314ee
Adam Reiter <adr5240@gmail.com>
2017-02-15 14:19:11 -0500

    Fix spacing

--- frontend/components/search_box.jsx
+++ frontend/components/search_box.jsx
@@ -14,158 +14,158 @@ String.prototype.contains = function(substring) { return this.indexOf(substring)
 
 const Search = React.createClass({
 
-  getInitialState: function () {
-    return ({ inputVal: '', modalOpen: false, modalObject: "" });
-  },
+    getInitialState: function () {
+        return ({ inputVal: '', modalOpen: false, modalObject: "" });
+    },
 
-  componentWillMount: function () {
-    this.authors = {};
-    this.books = {};
-    this.searchItems = [];
-    AuthorActions.fetchAllAuthors();
-    this.authorListener = AuthorStore.addListener(this._onChange);
-    this.bookListener = BookStore.addListener(this._onChange);
-  },
+    componentWillMount: function () {
+        this.authors = {};
+        this.books = {};
+        this.searchItems = [];
+        AuthorActions.fetchAllAuthors();
+        this.authorListener = AuthorStore.addListener(this._onChange);
+        this.bookListener = BookStore.addListener(this._onChange);
+    },
 
-  componentWillUnmount: function () {
-    this.bookListener.remove();
-    this.authorListener.remove();
-  },
+    componentWillUnmount: function () {
+        this.bookListener.remove();
+        this.authorListener.remove();
+    },
 
-  _addBook: function (book) {
-      BookActions.createBook(book);
-  },
+    _addBook: function (book) {
+        BookActions.createBook(book);
+    },
 
-  _clearSearchBox: function () {
-      this.searchItems = [];
-      this.setState({ inputVal: '' });
-  },
+    _clearSearchBox: function () {
+        this.searchItems = [];
+        this.setState({ inputVal: '' });
+    },
 
-  _onChange: function () {
-    this.books = BookStore.all();
-    this.authors = AuthorStore.all();
-    this.searchArray = this._resetSearchArray();
-  },
+    _onChange: function () {
+        this.books = BookStore.all();
+        this.authors = AuthorStore.all();
+        this.searchArray = this._resetSearchArray();
+    },
 
-  _handleInput: function (e) {
-    this.setState({ inputVal: e.currentTarget.value });
-    if(e.currentTarget.value.length > 1) {
-      this._selectItem(e.currentTarget.value);
-    } else {
-      this.searchItems = [];
-    }
-  },
-
-  _handleModal: function () {
-      this._clearSearchBox();
-      if(SessionsStore.isUserLoggedIn()) {
-          this.setState({ modalOpen: true });
-      } else {
-          this.forceUpdate();
-          hashHistory.push('/login');
-          setTimeout(() => alert("You must be logged in to add books!"), 200);
-      }
-  },
-
-  _onModalClose: function () {
-      ModalStyle.content.opacity = 0;
-      this.setState({ modalOpen: false });
-  },
-
-  _onModalOpen: function () {
-      ModalStyle.content.opacity = 100;
-  },
-
-  _resetSearchArray: function () {
-      let searchArray = [];
-
-      for (let book in this.books) {
-          searchArray.push(this.books[book]);
-      }
-
-      for (let author in this.authors) {
-          searchArray.push(this.authors[author]);
-      }
-      return searchArray;
-  },
-
-  _selectItem: function (input) {
-    this.searchItems = this.searchArray.filter((item) => {
-      if (item.title) {
-        if (item.title.toLowerCase().contains(input.toLowerCase())) {
-          return item;
+    _handleInput: function (e) {
+        this.setState({ inputVal: e.currentTarget.value });
+        if(e.currentTarget.value.length > 1) {
+            this._selectItem(e.currentTarget.value);
+        } else {
+            this.searchItems = [];
         }
-      } else if (item.full_name) {
-        if (item.full_name.toLowerCase().contains(input.toLowerCase())) {
-          return item;
+    },
+
+    _handleModal: function () {
+        this._clearSearchBox();
+        if(SessionsStore.isUserLoggedIn()) {
+            this.setState({ modalOpen: true });
+        } else {
+            this.forceUpdate();
+            hashHistory.push('/login');
+            setTimeout(() => alert("You must be logged in to add books!"), 200);
         }
-      }
-    });
-  },
+    },
 
-  render: function () {
-    this.results = [];
+    _onModalClose: function () {
+        ModalStyle.content.opacity = 0;
+        this.setState({ modalOpen: false });
+    },
 
-    if(this.searchItems.length > 0) {
-      this.searchItems.forEach((item) => {
-        if (item.title) {
-          this.results.push(
-            <li key={item.title} onClick={this._clearSearchBox}>
-              <Link to={`/books/${item.id}`}
-                    className='book-img'><img src={item.book_cover_url}/>
-              </Link>
-              <div>
-                <Link to={`/books/${item.id}`} className='search-title' >{item.title}</Link>
-                <br/>
-                <Link to={`/authors/${item.author_id}`} className='book-author'>{item.author}</Link>
-              </div>
-            </li>
-          );
-        } else if (item.full_name) {
-          this.results.push(
-            <li key={item.full_name} onClick={this._clearSearchBox}>
-              <Link to={`/authors/${item.id}`}
-                    className='author-img'><img src={item.image_url}/>
-              </Link>
-              <div>
-                <Link to={`/authors/${item.id}`} className='search-title' >{item.full_name}</Link>
-              </div>
-            </li>
-          );
+    _onModalOpen: function () {
+        ModalStyle.content.opacity = 100;
+    },
+
+    _resetSearchArray: function () {
+        let searchArray = [];
+
+        for (let book in this.books) {
+            searchArray.push(this.books[book]);
         }
-      });
-    } else if (this.state.inputVal.length > 2) {
-      this.results.push(
-        <li className='not-found' key='not-found' onClick={ this._handleModal }>
-          Book not found... Click here to add it!
-        </li>
-      );
-    }
 
-    return (
-      <div className="search-box">
-        <input onChange={this._handleInput}
-               type="text"
-               name="searchBox"
-               value={this.state.inputVal}
-               placeholder='Search...' />
-        <ul className='search-results'>
-          {this.results}
-        </ul>
+        for (let author in this.authors) {
+            searchArray.push(this.authors[author]);
+        }
+        return searchArray;
+    },
 
-        <Modal
-          isOpen={this.state.modalOpen}
-          onRequestClose={this._onModalClose}
-          onAfterOpen={this._onModalOpen}
-          style={ ModalStyle }>
+    _selectItem: function (input) {
+        this.searchItems = this.searchArray.filter((item) => {
+            if (item.title) {
+                if (item.title.toLowerCase().contains(input.toLowerCase())) {
+                    return item;
+                }
+            } else if (item.full_name) {
+                if (item.full_name.toLowerCase().contains(input.toLowerCase())) {
+                return item;
+                }
+            }
+        });
+    },
 
-          <button onClick={this._onModalClose}>Close</button>
-          <br/>
-          <AddBook close={this._onModalClose}/>
-        </Modal>
-      </div>
-    );
-  }
+    render: function () {
+        this.results = [];
+
+        if(this.searchItems.length > 0) {
+            this.searchItems.forEach((item) => {
+                if (item.title) {
+                    this.results.push(
+                        <li key={item.title} onClick={this._clearSearchBox}>
+                            <Link to={`/books/${item.id}`}
+                                  className='book-img'><img src={item.book_cover_url}/>
+                            </Link>
+                            <div>
+                                <Link to={`/books/${item.id}`} className='search-title' >{item.title}</Link>
+                                <br/>
+                                <Link to={`/authors/${item.author_id}`} className='book-author'>{item.author}</Link>
+                            </div>
+                        </li>
+                    );
+                } else if (item.full_name) {
+                    this.results.push(
+                        <li key={item.full_name} onClick={this._clearSearchBox}>
+                            <Link to={`/authors/${item.id}`}
+                                  className='author-img'><img src={item.image_url}/>
+                            </Link>
+                            <div>
+                                <Link to={`/authors/${item.id}`} className='search-title' >{item.full_name}</Link>
+                            </div>
+                        </li>
+                    );
+                }
+            });
+        } else if (this.state.inputVal.length > 2) {
+            this.results.push(
+                <li className='not-found' key='not-found' onClick={ this._handleModal }>
+                    Book not found... Click here to add it!
+                </li>
+            );
+        }
+
+        return (
+            <div className="search-box">
+                <input onChange={this._handleInput}
+                       type="text"
+                       name="searchBox"
+                       value={this.state.inputVal}
+                       placeholder='Search...' />
+                    <ul className='search-results'>
+                        {this.results}
+                    </ul>
+
+                    <Modal
+                        isOpen={this.state.modalOpen}
+                        onRequestClose={this._onModalClose}
+                        onAfterOpen={this._onModalOpen}
+                        style={ ModalStyle }>
+
+                        <button onClick={this._onModalClose}>Close</button>
+                        <br/>
+                        <AddBook close={this._onModalClose}/>
+                    </Modal>
+                </div>
+            );
+        }
 
 });
 
